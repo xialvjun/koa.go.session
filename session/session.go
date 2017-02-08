313385f5a57925775cc1cb734cdde5d8d064b42c
xialvjun <xialvjun@live.com>
2017-02-08 16:36:43 +0800

    init

---
+++ session/session.go
@@ -0,0 +1,44 @@
+package session
+
+import (
+	"net/http"
+
+	"github.com/xialvjun/koa.go/koa"
+	uuid "other/github.com/satori/go.uuid"
+)
+
+var key string
+var mem = make(map[string]interface{})
+
+func Middleware(setkey string) koa.Middleware {
+	key = setkey
+	return func(w http.ResponseWriter, r *http.Request, next koa.Next) {
+		sid, error := r.Cookie(key)
+		if error != nil || sid.Value == "" {
+			uid := uuid.NewV4().String()
+			w.Header().Set("Set-Cookie", "sid="+uid)
+			r.AddCookie(&http.Cookie{Name: key, Value: uid})
+		}
+	}
+}
+
+func Set(r *http.Request, value interface{}) error {
+	sid, error := r.Cookie(key)
+	if error != nil {
+		return error
+	}
+	mem[sid.Value] = value
+	return nil
+}
+
+func Get(r *http.Request) (interface{}, error) {
+	sid, err := r.Cookie(key)
+	if err != nil {
+		return nil, err
+	}
+	value, ok := mem[sid.Value]
+	if ok {
+		return value, nil
+	}
+	return nil, nil
+}
